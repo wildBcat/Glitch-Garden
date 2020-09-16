78ddde54c16fc494b9c68c4b069f07ff6e431dc7
wildBcat <sundance.eaton@icloud.com>
2020-09-16 10:11:20 -0600

    Initial commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int TimeToWait = 3;
+
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(SplashScreenDelay());
+        }
+    }
+
+    private IEnumerator SplashScreenDelay()
+    {
+        yield return new WaitForSeconds(TimeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
